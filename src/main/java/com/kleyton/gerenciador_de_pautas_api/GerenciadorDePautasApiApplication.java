82c1d8092d479add6a0d6af3aca91b7cbb8012c5
KleytonFMadruga <kleytonmadruga925@gmail.com>
2024-06-07 13:07:16 -0300

    Base do projeto

---
+++ src/main/java/com/kleyton/gerenciador_de_pautas_api/GerenciadorDePautasApiApplication.java
@@ -0,0 +1,13 @@
+package com.kleyton.gerenciador_de_pautas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDePautasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDePautasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kleyton/gerenciador_de_pautas_api/GerenciadorDePautasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kleyton.gerenciador_de_pautas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDePautasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
